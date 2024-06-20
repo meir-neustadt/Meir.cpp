8cb5793ff5e33d17514b661da46c9bb5f4a30e70
MeirNeustadt <msn.binah.1@gmail.com>
2024-06-20 22:49:24 +0300

    -

--- stroustrup/class.cpp
+++ stroustrup/class.cpp
@@ -6,17 +6,18 @@ class Vector
 {
 public:
     Vector(int s) : elem{new float[s]}, sz{s} {}
-    float &operator[](int i) { return elem[i]; }
+    float& operator[](int i) { return elem[i]; }
     int size() { return sz; }
 
-    // private:
+private:
     float *elem; // pointer to the elements
     int sz;      // the number of elements
 };
 
+
 int main()
 {
     Vector v(5);
-    v.elem[2] = 5.789;
-    cout << v[2];
+    v[1] = 5.789;
+    cout << v[1] << '\n';
 }
