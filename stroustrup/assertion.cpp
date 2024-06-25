6731936194bd6b934f1386f33e3ddb3169a72dd0
MeirNeustadt <msn.binah.1@gmail.com>
2024-06-25 14:08:45 +0300

    assertions

---
+++ stroustrup/assertion.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+constexpr float C = 299792.458; // Speed of light in km/s
+
+void f()
+{
+    constexpr float local_max = 160.0 / (60 * 60); // Convert 160 km/h to km/s
+    static_assert(local_max < C, "can't go that fast"); 
+    static_assert(sizeof(int)==8, "sizof int isn't 8");
+}
+
+int main(){
+   f();
+   std::cout << "Function executed successfully.\n";
+   return 0;
+}
