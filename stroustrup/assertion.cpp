6072dba8402991571cd6c5ff1979742789a58c34
MeirNeustadt <msn.binah.1@gmail.com>
2024-06-25 14:14:07 +0300

    assertions

--- stroustrup/assertion.cpp
+++ stroustrup/assertion.cpp
@@ -6,7 +6,7 @@ void f()
 {
     constexpr float local_max = 160.0 / (60 * 60); // Convert 160 km/h to km/s
     static_assert(local_max < C, "can't go that fast"); 
-    static_assert(sizeof(int)==8, "sizof int isn't 8");
+    static_assert(sizeof(int)==4, "sizof int isn't 8");
 }
 
 int main(){
